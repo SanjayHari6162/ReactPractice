67ff144799fc29d195fc8c251cf871b3f80b2194
SanjayHari6162 <sanjayhari6162@gmail.com>
2024-09-26 06:42:22 +0000

    home page finished

--- FoodKart/src/Product.jsx
+++ FoodKart/src/Product.jsx
@@ -1,10 +1,20 @@
 
 
-export const Product = () =>{
+export const Product = ({data}) =>{
     
     return(
-        <div>
-            Products of the page
+        <div className="container">
+            {data.map((item)=>(<div className='home-product' key={item.id}>
+                <div className="home-product-img">
+                    <img src={item.pic}></img>
+                </div>
+
+                <div className="home-product-detail">
+                <h2>{item.name}</h2>
+                <p>Price: Rs.{item.price}</p>
+                <button>Add to cart</button>
+                </div>
+            </div>))}
         </div>
     )
 }
